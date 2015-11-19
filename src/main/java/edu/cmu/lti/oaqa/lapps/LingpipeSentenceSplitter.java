a7837896c8e15b7cc73bbfe6b99d4d78b93ebca9
Keith Suderman <suderman@cs.vassar.edu>
2015-11-19 18:12:48 -0500

    Fixed typo in sentence splitter name.

---
+++ src/main/java/edu/cmu/lti/oaqa/lapps/LingpipeSentenceSplitter.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2014 The Language Application Grid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package edu.cmu.lti.oaqa.lapps;
+
+import com.aliasi.chunk.Chunk;
+import com.aliasi.chunk.Chunking;
+import com.aliasi.sentences.IndoEuropeanSentenceModel;
+import com.aliasi.sentences.SentenceChunker;
+import com.aliasi.sentences.SentenceModel;
+import com.aliasi.tokenizer.IndoEuropeanTokenizerFactory;
+import com.aliasi.tokenizer.Tokenizer;
+import com.aliasi.tokenizer.TokenizerFactory;
+import org.lappsgrid.api.ProcessingService;
+import org.lappsgrid.discriminator.Discriminators;
+import org.lappsgrid.metadata.IOSpecification;
+import org.lappsgrid.metadata.ServiceMetadata;
+import org.lappsgrid.serialization.Data;
+import org.lappsgrid.serialization.DataContainer;
+import org.lappsgrid.serialization.Serializer;
+import org.lappsgrid.serialization.lif.Annotation;
+import org.lappsgrid.serialization.lif.Container;
+import org.lappsgrid.serialization.lif.View;
+import org.lappsgrid.vocabulary.Features;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.lappsgrid.discriminator.Discriminators.Uri;
+
+public class LingpipeSentenceSplitter extends AbstractLingpipeService {
+
+    static final SentenceModel SENTENCE_MODEL  = new IndoEuropeanSentenceModel();
+    static final TokenizerFactory TOKENIZER_FACTORY = IndoEuropeanTokenizerFactory.INSTANCE;
+    static final SentenceChunker SENTENCE_CHUNKER = new SentenceChunker(TOKENIZER_FACTORY,SENTENCE_MODEL);
+
+
+    public LingpipeSentenceSplitter(){
+        super();
+
+        metadata.setDescription("Lingpipe IndoEuropean Tokenizer");
+
+        // JSON for input information
+        IOSpecification requires = new IOSpecification();
+        requires.addFormats(Uri.TEXT, Uri.LAPPS);
+        requires.addLanguage("en");             // Source language
+        //requires.addAnnotation(Uri.TOKEN);
+
+        // JSON for output information
+        IOSpecification produces = new IOSpecification();
+        produces.addFormat(Uri.LAPPS);          // LIF (form)
+        produces.addAnnotation(Uri.SENTENCE);
+        requires.addLanguage("en");             // Target language
+
+        // Embed I/O metadata JSON objects
+        metadata.setRequires(requires);
+        metadata.setProduces(produces);
+    }
+
+    @Override
+    public String execute(String input) {
+        // Step #1: Parse the input.
+        Data data = Serializer.parse(input, Data.class);
+
+        // Step #2: Check the discriminator
+        final String discriminator = data.getDiscriminator();
+        if (discriminator.equals(Uri.ERROR)) {
+            // Return the input unchanged.
+            return input;
+        }
+
+        // Step #3: Extract the token.
+        Container container = null;
+        if (discriminator.equals(Uri.TEXT)) {
+            container = new Container();
+            container.setText(data.getPayload().toString());
+        } else if (discriminator.equals(Uri.LAPPS)) {
+            container = new Container((Map) data.getPayload());
+        } else {
+            // This is a format we don't accept.
+            String message = String.format("Unsupported discriminator type: %s", discriminator);
+            return new Data<String>(Uri.ERROR, message).asJson();
+        }
+
+//        List<View> views = container.findViewsThatContain(Uri.TOKEN);
+//        if (views == null || views.size() == 0)
+//        {
+//            return new Data<String>(Uri.ERROR, "Unable to process input: no tokens found").asJson();
+//        }
+//        View tokenStep = views.get(0);
+//        List<Annotation> annotations = tokenStep.getAnnotations();
+
+        String text = container.getText();
+        if (text == null || text.isEmpty()) {
+            return input;
+        }
+
+        // Step #4: Create a new View
+        View view = container.newView();
+
+        // Step #5: Chuck the text and add annotations.
+//        for (Annotation annotation : annotations) {
+//            String token
+//                    = text.substring(annotation.getStart().intValue(), (int) annotation.getEnd().intValue());
+//        }
+
+        Chunking chunking = SENTENCE_CHUNKER.chunk(text.toCharArray(), 0, text.length());
+        Set<Chunk> sentences = chunking.chunkSet();
+        int i = 1;
+        for (Iterator<Chunk> it = sentences.iterator(); it.hasNext(); ) {
+            Chunk sentence = it.next();
+            int start = sentence.start();
+            int end = sentence.end();
+            Annotation a = view.newAnnotation("lingpipe-sentence-" + i, Uri.SENTENCE, start, end);
+            a.setLabel(Discriminators.Alias.SENTENCE);
+        }
+
+        // Step #6: Update the view's metadata. Each view contains metadata about the
+        // annotations it contains, in particular the name of the tool that produced the
+        // annotations.
+        view.addContains(Uri.SENTENCE, this.getClass().getName(), "tokenizer:lingpipe-indo-european-tokenizer");
+
+        // Step #7: Create a DataContainer with the result.
+        data = new DataContainer(container);
+
+        // Step #8: Serialize the data object and return the JSON.
+        return data.asJson();
+
+    }
+
+}
